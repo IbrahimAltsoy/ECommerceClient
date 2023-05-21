6c54d1d17fdac99b36f3bb23ef96b83252491cd5
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-05-21 14:26:54 -0700

    adminModule added compnents

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from './layout/layout.module';
 import { LayoutComponent } from './layout/layout.component';
+import { ComponentsModule } from './components/components.module';
+
 
 
 
@@ -9,7 +11,9 @@ import { LayoutComponent } from './layout/layout.component';
   declarations: [],
   imports: [
     CommonModule,
-    LayoutModule
+    LayoutModule,
+    ComponentsModule
+
   ],
   exports:[
     LayoutModule
