6f0fc690101bbbf3f76441d9e85de3ccd8ed44b8
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2023-05-21 23:54:55 -0700

    added router and try router

--- src/app/admin/components/customers/customers.module.ts
+++ src/app/admin/components/customers/customers.module.ts
@@ -12,7 +12,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     RouterModule.forChild([
-      {path: "X", component:CustomersComponent}
+      {path: "", component:CustomersComponent}
       // {path:"customerblabla", component:CustomersComponent} ;eklinde rooter eklenir
     ])
   ]
